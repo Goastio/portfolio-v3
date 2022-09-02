4ecaf29538d1f47d31ecc3870fe130f3ce494d35
Goastio <seventylife@gmail.com>
2022-09-02 17:11:11 -0400

    implemented footer structure

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -1,7 +1,87 @@
+import { FaCloudsmith } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <section className="bg-gray-100 py-20 px-5">
-      <div className="max-w-screen-lg mx-auto">foot</div>
+      <div className="max-w-screen-lg mx-auto flex flex-row flex-wrap">
+        <div className="flex flex-col">
+          <div className="flex font-semibold tracking-tight text-lg items-center">
+            <FaCloudsmith className="text-indigo-500" />
+            Brandon Maczynski
+          </div>
+          <div className="flex text-sm">Frontend Developer</div>
+        </div>
+        <div className="flex w-full justify-between mt-5">
+          <div className="flex flex-col">
+            <div className="flex font-semibold tracking-tight text-lg">
+              Work
+            </div>
+            <div className="text-sm flex flex-col">
+              <a
+                href="/reddit-browser"
+                target="_blank"
+                className="text-black text-opacity-80 hover:text-opacity-100"
+              >
+                Reddit Browser
+              </a>
+              <a
+                href="/blackjack"
+                target="_blank"
+                className="text-black text-opacity-80 hover:text-opacity-100"
+              >
+                Blackjack
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex font-semibold tracking-tight text-lg">
+              Socials
+            </div>
+            <div className="text-sm flex flex-col">
+              <a
+                href="https://github.com/Goastio"
+                rel="noreferrer"
+                target="_blank"
+                className="text-black text-opacity-80 hover:text-opacity-100"
+              >
+                Github
+              </a>
+              <a
+                href="https://www.linkedin.com/in/brandon-maczynski-40428022a/"
+                rel="noreferrer"
+                target="_blank"
+                className="text-black text-opacity-80 hover:text-opacity-100"
+              >
+                LinkedIn
+              </a>
+              <a
+                href="https://twitter.com/DevelopWebsites"
+                rel="noreferrer"
+                target="_blank"
+                className="text-black text-opacity-80 hover:text-opacity-100"
+              >
+                Twitter
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex font-semibold tracking-tight text-lg">
+              Information
+            </div>
+            <div className="text-sm">
+              <a
+                href="mailto:bmaczynski11@yahoo.com"
+                className="flex text-black text-opacity-80 hover:text-opacity-100"
+              >
+                bmaczynski11@yahoo.com
+              </a>
+              <div className="flex text-black text-opacity-80 hover:text-opacity-100">
+                Resume
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
