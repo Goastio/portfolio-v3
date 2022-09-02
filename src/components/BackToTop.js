6c89643d16ea581528e9176504033d2c0e948945
Goastio <seventylife@gmail.com>
2022-09-02 17:59:23 -0400

    Added back to top button to footer

---
+++ src/components/BackToTop.js
@@ -0,0 +1,13 @@
+import { BsArrowUpShort } from "react-icons/bs";
+const BackToTop = () => {
+  return (
+    <div className="flex">
+      <div className="flex items-center">
+        <BsArrowUpShort className="text-xl" />
+        Back to the top
+      </div>
+    </div>
+  );
+};
+
+export default BackToTop;
